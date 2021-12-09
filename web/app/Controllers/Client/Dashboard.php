2ddee7ba35a2197954a6cf221b52f9e2aebb9cc3
Ibnurizqiaramadan <dadanibnu61@gmail.com>
2021-12-09 16:41:00 +0700

    add controller for client dashboard

---
+++ web/app/Controllers/Client/Dashboard.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers\Client;
+
+use App\Controllers\BaseController;
+
+class Dashboard extends BaseController
+{
+    public function index()
+    {
+        return view('client/dashboard');
+    }
+}
