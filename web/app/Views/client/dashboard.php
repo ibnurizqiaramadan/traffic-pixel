46bc02f8ced40dfb82ebdd86f571343300a6c48b
Ibnurizqiaramadan <dadanibnu61@gmail.com>
2021-12-09 16:41:29 +0700

    add view dashboard

---
+++ web/app/Views/client/dashboard.php
@@ -0,0 +1,12 @@
+<?= $this->extend('Client/layouts/app'); ?>
+
+<?= $this->section('content') ?>
+<div class="row">
+    <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+        <div class="card">
+            hello
+        </div>
+    </div>
+</div>
+
+<?= $this->endSection() ?>
