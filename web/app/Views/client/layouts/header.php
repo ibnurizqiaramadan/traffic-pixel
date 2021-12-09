861dc3352126871e0339aab760b3954d3e3e346b
Ibnurizqiaramadan <dadanibnu61@gmail.com>
2021-12-09 18:01:14 +0700

    bruh nevermind, remove .min

--- web/app/Views/client/layouts/header.php
+++ web/app/Views/client/layouts/header.php
@@ -19,7 +19,7 @@
     <!-- Material Icons -->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
     <!-- CSS Files -->
-    <link id="pagestyle" href="<?= base_url() ?>/assets/client/assets/css/material-dashboard.min.css?v=3.0.0" rel="stylesheet" />
+    <link id="pagestyle" href="<?= base_url() ?>/assets/client/assets/css/material-dashboard.css?v=3.0.0" rel="stylesheet" />
 
     <?= $this->include('client/layouts/js'); ?>
 
