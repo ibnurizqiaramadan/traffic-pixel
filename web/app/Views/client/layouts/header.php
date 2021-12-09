645e8596c36226e4281d9a71f0bbb41fc87bac5d
Ibnurizqiaramadan <dadanibnu61@gmail.com>
2021-12-09 17:50:00 +0700

    remove custom.css

--- web/app/Views/client/layouts/header.php
+++ web/app/Views/client/layouts/header.php
@@ -20,7 +20,6 @@
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
     <!-- CSS Files -->
     <link id="pagestyle" href="<?= base_url() ?>/assets/client/assets/css/material-dashboard.min.css?v=3.0.0" rel="stylesheet" />
-    <link href="<?= base_url() ?>/assets/client/custom.css" rel="stylesheet" />
 
     <?= $this->include('client/layouts/js'); ?>
 
