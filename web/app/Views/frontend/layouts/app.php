fd4da1bbd47fac3cafdcaba9470f5cc3403d99aa
Ibnurizqiaramadan <dadanibnu61@gmail.com>
2021-12-15 20:17:05 +0700

    move footer to layouts section

--- web/app/Views/frontend/layouts/app.php
+++ web/app/Views/frontend/layouts/app.php
@@ -2,3 +2,4 @@
 <?= $this->include('frontend/layouts/nav'); ?>
 <?= $this->include('frontend/layouts/js'); ?>
 <?= $this->renderSection('content'); ?>
+<?= $this->include('frontend/layouts/footer'); ?>
---
+++ web/app/Views/frontend/layouts/footer.php
@@ -0,0 +1,32 @@
+<!-- Footer-->
+<footer class="footer text-center">
+    <div class="container">
+        <div class="row">
+            <h4 class="text-uppercase mb-5">Contacts</h4>
+            <!-- Footer Location-->
+            <div class="col-lg-6 mb-6 mb-lg-0">
+                <h4 class="mb-4 fs-5">Ibnu Rizqia Ramadan</h4>
+                <a class="btn btn-outline-light btn-social mx-1" href="https://github.com/ibnurizqiaramadan" target="_blank"><i class="fab fa-fw fa-github"></i></a>
+                <a class="btn btn-outline-light btn-social mx-1" href="https://www.linkedin.com/in/ibnu-r-baa04010a/" target="_blank"><i class="fab fa-fw fa-linkedin-in"></i></a>
+                <a class="btn btn-outline-light btn-social mx-1" href="https://discord.com/users/257147179297144833" target="_blank"><i class="fab fa-fw fa-discord"></i></a>
+                <br />
+            </div>
+            <!-- Footer Social Icons-->
+            <div class="col-lg-6 mb-6 mb-lg-0">
+                <h4 class="mb-4 fs-5">Nanda Vian N</h4>
+                <a class="btn btn-outline-light btn-social mx-1" href="https://github.com/Nanda-Vian" target="_blank"><i class="fab fa-fw fa-github"></i></a>
+                <a class="btn btn-outline-light btn-social mx-1" href="https://www.linkedin.com/in/nanda-vian" target="_blank"><i class="fab fa-fw fa-linkedin-in"></i></a>
+                <a class="btn btn-outline-light btn-social mx-1" href="https://discord.com/users/426246797447528458" target="_blank"><i class="fab fa-fw fa-discord"></i></a>
+                <br />
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- Copyright Section-->
+<div class="copyright py-4 text-center text-white">
+    <div class="container"><small>Copyright &copy; Traffic Pixel 2021</small></div>
+</div>
+<!-- Bootstrap core JS-->
+</body>
+
+</html>
