000c0c8747a44a98412cbd4c7564e339193f0028
Ibnurizqiaramadan <dadanibnu61@gmail.com>
2021-12-24 21:47:07 +0700

    change footer

--- web/app/Views/frontend/layouts/footer.php
+++ web/app/Views/frontend/layouts/footer.php
@@ -2,9 +2,9 @@
 <footer class="footer text-center">
     <div class="container">
         <div class="row">
-            <h4 class="text-uppercase mb-5">Contacts</h4>
+            <h4 class="text-uppercase mb-2">Contacts</h4>
             <!-- Footer Location-->
-            <div class="col-lg-6 mb-6 mb-lg-0">
+            <div class="col-lg-6 mb-6 mb-lg-0 mt-5">
                 <h4 class="mb-4 fs-5">Ibnu Rizqia Ramadan</h4>
                 <a class="btn btn-outline-light btn-social mx-1" href="https://github.com/ibnurizqiaramadan" target="_blank"><i class="fab fa-fw fa-github"></i></a>
                 <a class="btn btn-outline-light btn-social mx-1" href="https://www.linkedin.com/in/ibnu-r-baa04010a/" target="_blank"><i class="fab fa-fw fa-linkedin-in"></i></a>
@@ -12,7 +12,7 @@
                 <br />
             </div>
             <!-- Footer Social Icons-->
-            <div class="col-lg-6 mb-6 mb-lg-0">
+            <div class="col-lg-6 mb-6 mb-lg-0 mt-5">
                 <h4 class="mb-4 fs-5">Nanda Vian N</h4>
                 <a class="btn btn-outline-light btn-social mx-1" href="https://github.com/Nanda-Vian" target="_blank"><i class="fab fa-fw fa-github"></i></a>
                 <a class="btn btn-outline-light btn-social mx-1" href="https://www.linkedin.com/in/nanda-vian" target="_blank"><i class="fab fa-fw fa-linkedin-in"></i></a>
